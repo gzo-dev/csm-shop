5fe3bc0a0eede23338bc3181bbe8f77888fcdfc5
thuyhoang11 <gianglamweb@gmail.com>
2023-07-04 18:44:26 +0700

    commit

--- backend/src/models/contact.js
+++ backend/src/models/contact.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const contact = sequelize.define('contact', {
     name: DataTypes.STRING,
     // date_send: DataTypes.STRING,
+    phone: DataTypes.STRING,
     content: DataTypes.STRING,
     email: DataTypes.STRING,
     status: DataTypes.STRING,
