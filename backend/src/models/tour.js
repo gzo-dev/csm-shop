381d67dd23d48e87145f615c8009cd3bba1a71e7
fptcsm <gianglamweb@gmail.com>
2024-02-25 21:29:40 +0700

    commit

--- backend/src/models/tour.js
+++ backend/src/models/tour.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     type: DataTypes.INTEGER,
     content: DataTypes.TEXT,
     price: DataTypes.INTEGER,
+    kindof: DataTypes.INTEGER
   }, {});
   return tour;
 };
