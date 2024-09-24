858543153aa344c5215a46406af525377575d0ac
Hello <datistpham@gmail.com>
2024-09-24 22:05:06 +0700

    commit

--- serverside-fe/server.js
+++ serverside-fe/server.js
@@ -21,7 +21,7 @@ app.get("*", async (req, res) => {
 
     // Dữ liệu động từ server, có thể lấy từ DB hoặc API khác
     if (req.originalUrl.includes("/product")) {
-      console.log(req.originalUrl)
+      console.log(req.originalUrl.split("/")[2])
       const response = await axios({
         url: "https://api.minhkhanggroup.vn/api/v1/product/serverside/d?id=" + req.originalUrl.split("/")[2].split("-").at(-1),
         method: "get",
